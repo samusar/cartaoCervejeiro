e2f76d7d88b1298e021550cb835290425eaebbc6
Samuel Henrique <samu.henr@gmail.com>
2019-07-30 09:39:50 -0300

    Adição de linguagem pt-br

---
+++ resources/lang/pt-br/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'A senha deve ter, pelo menos, seis caracteres e ser igual a de confirmação.',
+    'reset' => 'Sua senha foi alterada com sucesso!',
+    'sent' => 'Nós enviamos um e-mail com o seu link de redefinição de senha!',
+    'token' => 'Este token de redefinição de senha é invalido.',
+    'user' => "Não conseguimos encontrar um usuário com esse endereço de e-mail.",
+
+];
